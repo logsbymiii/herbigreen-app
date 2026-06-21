17fc1aa6b0f15db674ce310cf6ffd55cac99fce3
helmi <137972846+teathricalhood122@users.noreply.github.com>
2026-06-22 03:20:22 +0700

    Fix getColumns error in AbsensiDashboard view

--- resources/views/filament/pages/absensi-dashboard.blade.php
+++ resources/views/filament/pages/absensi-dashboard.blade.php
@@ -1,7 +1,2 @@
 <x-filament-panels::page>
-    <x-filament-widgets::widgets
-        :columns="$this->getColumns()"
-        :data="$this->getWidgetData()"
-        :widgets="$this->getVisibleWidgets()"
-    />
 </x-filament-panels::page>

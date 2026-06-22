ad83672f00385b93f52407d5a187db2790a35d8f
helmi <137972846+teathricalhood122@users.noreply.github.com>
2026-06-22 23:22:05 +0700

    Ensure columns property is set in TodaySummaryWidget

--- app/Filament/Widgets/TodaySummaryWidget.php
+++ app/Filament/Widgets/TodaySummaryWidget.php
@@ -13,6 +13,7 @@ class TodaySummaryWidget extends StatsOverviewWidget
 {
     protected static ?int $sort = 1;
     protected int | string | array $columnSpan = 'full';
+    protected int | string | array $columns = 4;
 
     protected function getColumns(): int
     {

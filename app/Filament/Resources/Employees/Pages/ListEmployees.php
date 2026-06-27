89a599dc57710194fe1acc82aece604b04be6b5c
helmi <137972846+teathricalhood122@users.noreply.github.com>
2026-06-28 00:40:52 +0700

    Enhancement: Add Export Excel button for Employees

--- app/Filament/Resources/Employees/Pages/ListEmployees.php
+++ app/Filament/Resources/Employees/Pages/ListEmployees.php
@@ -13,6 +13,15 @@ class ListEmployees extends ListRecords
     protected function getHeaderActions(): array
     {
         return [
+            \pxlrbt\FilamentExcel\Actions\Pages\ExportAction::make()
+                ->label('Export Excel')
+                ->color('success')
+                ->icon('heroicon-o-document-arrow-down')
+                ->exports([
+                    \pxlrbt\FilamentExcel\Exports\ExcelExport::make()
+                        ->fromTable()
+                        ->withFilename('Export_Karyawan_' . date('Y-m-d'))
+                ]),
             CreateAction::make(),
         ];
     }

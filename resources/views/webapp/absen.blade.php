3b6f411773d2f9ca10eae0f1e14274c424d36d27
helmi <137972846+teathricalhood122@users.noreply.github.com>
2026-07-02 14:52:44 +0700

    Compress native fallback image to prevent Payload Too Large crash

--- resources/views/webapp/absen.blade.php
+++ resources/views/webapp/absen.blade.php
@@ -283,11 +283,28 @@
             reader.onload = function(e) {
                 const img = new Image();
                 img.onload = function() {
-                    canvas.width = img.width;
-                    canvas.height = img.height;
+                    const MAX_WIDTH = 1024;
+                    const MAX_HEIGHT = 1024;
+                    let width = img.width;
+                    let height = img.height;
+
+                    if (width > height) {
+                        if (width > MAX_WIDTH) {
+                            height *= MAX_WIDTH / width;
+                            width = MAX_WIDTH;
+                        }
+                    } else {
+                        if (height > MAX_HEIGHT) {
+                            width *= MAX_HEIGHT / height;
+                            height = MAX_HEIGHT;
+                        }
+                    }
+
+                    canvas.width = width;
+                    canvas.height = height;
                     const ctx = canvas.getContext('2d');
-                    ctx.drawImage(img, 0, 0);
-                    customPhotoData = canvas.toDataURL('image/jpeg', 0.8);
+                    ctx.drawImage(img, 0, 0, width, height);
+                    customPhotoData = canvas.toDataURL('image/jpeg', 0.6);
                     takePhotoAndSubmit(true);
                 }
                 img.src = e.target.result;
